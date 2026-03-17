e10a09a979015655d3fde403a6ba8987e579d42e
Yisus2328 <je.johnson@duocuc.c>
2026-03-17 18:40:06 -0300

    modelos creados en la base de datos

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    precio = models.IntegerField() 
+    stock = models.IntegerField(default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    direccion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    total = models.IntegerField()
+
+    def __str__(self):
+        return f"Venta {self.id} - {self.cliente.nombre}"
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio_unitario = models.IntegerField() # Guardamos el precio del momento de la compra
